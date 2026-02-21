8d8bfffe6261fce9209dccf68ca50f1528bf5638
7rockstarmade <243180667+7rockstarmade@users.noreply.github.com>
2026-02-21 20:49:13 +0300

    style: create app colors

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyNewsApp extends StatelessWidget {
+  const MyNewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // brand
+  static const primary = Color.fromARGB(255, 24, 119, 242);
+  static const error = Color.fromARGB(255, 237, 46, 126);
+  static const success = Color.fromARGB(255, 0, 186, 136);
+  static const warning = Color.fromARGB(255, 244, 183, 64);
+
+  // light theme
+  static const titleActive = Color.fromARGB(255, 5, 5, 5);
+  static const bodyText = Color.fromARGB(255, 78, 75, 102);
+  static const buttonText = Color.fromARGB(255, 102, 112, 128);
+  static const placeholder = Color.fromARGB(255, 160, 163, 189);
+  static const button = Color.fromARGB(255, 238, 241, 244);
+  static const disabledInput = Color.fromARGB(255, 238, 241, 244);
+
+  // dark theme
+  static const darkBackground = Color.fromARGB(255, 28, 30, 33);
+  static const darkBodyText = Color.fromARGB(255, 176, 179, 184);
+  static const darkInput = Color.fromARGB(255, 58, 59, 60);
+  static const darkTitle = Color.fromARGB(255, 228, 230, 235);
+}
